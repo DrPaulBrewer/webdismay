268b9886a1a2d9099a65f3092ae2c09fec55347a
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2016-07-22 08:07:07 +0000

    try extending browser timeout to 60s

--- karma.conf.js
+++ karma.conf.js
@@ -72,6 +72,8 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Firefox'],
 
+    browserNoActivityTimeout: 60000,
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
